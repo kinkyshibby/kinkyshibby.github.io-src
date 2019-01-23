d568a06d9f1b56f8c9de84e538faa4a21344eb47
Sm00thB4by <sm00thb4by@gmail.com>
2019-01-23 00:09:04 +0000

    Support bundling on Windows

--- viewer/slimmer.py
+++ viewer/slimmer.py
@@ -4,8 +4,8 @@ import re
 file_path = './dist/index.html'
 file_content = ''
 
-with open(file_path, 'rt') as f:
+with open(file_path, 'rt', encoding="utf8") as f:
   file_content = f.read()
 
-with open(file_path, 'wt') as f:
+with open(file_path, 'wt', encoding="utf8") as f:
   f.write(re.sub(r'>\s+<', '><', file_content))
--- viewer/templater.py
+++ viewer/templater.py
@@ -4,6 +4,7 @@ import re
 input_file = './src/template.html'
 output_file = './src/index.html'
 
+
 def repl(m):
   name = m.group(1)
   path = in_file.name[:in_file.name.rindex('/') + 1] + name
@@ -18,10 +19,11 @@ def repl(m):
     prefix = f'<script type="text/javascript">\n'
     postfix = '\n</script>'
 
-  with open(path, 'rt') as f:
+  with open(path, 'rt', encoding="utf8") as f:
     return prefix + f.read() + postfix
 
+
 if __name__ == '__main__':
-  with open(output_file, 'wt') as out_file:
-    with open(input_file, 'rt') as in_file:
+  with open(output_file, 'wt', encoding="utf8") as out_file:
+    with open(input_file, 'rt', encoding="utf8") as in_file:
       out_file.write(re.sub(r'<!--import\s*([^<>]+)\s*-->', repl, in_file.read()))
