d4bb34095e8be0babf7fdfa46507d6733b623331
Sm00thB4by <sm00thb4by@gmail.com>
2019-01-29 01:23:45 +0000

    Add compact mode

--- viewer/src/index.js
+++ viewer/src/index.js
@@ -1,3 +1,30 @@
+let compact_mode = 'localStorage' in window && localStorage.compact || 0
+
+const get_compact_css = () => (compact_mode == 0 ? '' : `
+  .description, .tag-container {
+    padding: 0;
+    white-space: nowrap; 
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .play-count { display: none; }
+  .search-result {
+    margin: 0;
+    padding: .5em;
+    grid-gap: .5em;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: 1em 1.5em;
+    grid-template-areas:
+      "title tags"
+      "description description";
+  }
+  .search-result:nth-child(odd) {
+    background-color: #3c3c3c;
+  }
+`)
+
+let compact_css = get_compact_css()
+
 const input_element = document.getElementById('search_input')
 input_element.focus()
 
@@ -9,7 +36,7 @@ const get_multikeyword_selector = value => `.search-result${value.length === 0 ?
 
 const stylesheet = document.createElement('style')
 stylesheet.type = 'text/css'
-stylesheet.innerHTML = get_multikeyword_selector(input_element.value)
+stylesheet.innerHTML = compact_css + get_multikeyword_selector(input_element.value)
 document.head.appendChild(stylesheet)
 
 const debounce = (func, delay) => {
@@ -23,8 +50,15 @@ const debounce = (func, delay) => {
   }
 }
 
-input_element.addEventListener('input', debounce(() => stylesheet.innerHTML = get_multikeyword_selector(input_element.value.trim().toLowerCase()), 10))
+input_element.addEventListener('input', debounce(() => stylesheet.innerHTML = compact_css + get_multikeyword_selector(input_element.value.trim().toLowerCase()), 10))
 document.getElementById('button_random').addEventListener('click', () => stylesheet.innerHTML = `.search-result:nth-child(${Math.trunc(Math.random() * document.getElementsByClassName('search-result').length) + 1}) { display: grid !important; }`)
+
+document.getElementById('button_compact').addEventListener('click', () => {
+  compact_mode ^= 1
+  'localStorage' in window && (localStorage.compact = compact_mode)
+  compact_css = get_compact_css()
+  input_element.dispatchEvent(new Event('input'), { 'bubbles': true })
+})
 
 window.append_tag = ev => {
   input_element.value += `${input_element.value.trim().length === 0 ? '' : ', '}${(ev.target || ev.srcElement).innerHTML}`
